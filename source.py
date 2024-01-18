db9128f270bb83317f394777097255bd912f409e
kasparsrincs <143948256+kasparsrincs@users.noreply.github.com>
2024-01-18 21:19:41 +0000

    Pilns kods

--- source.py
+++ source.py
@@ -3,10 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
-
+import pyautogui
 
 parole = "lkjlkj12"
 lvards = "janisprogrametajs12"
+bilde = r"C:\Users\Kaspars R\Documents\kakis\lauwa.jpg"
+bildes_apraksts="Liels kakis"
+
+
+
 
 
 service = Service()
@@ -33,36 +38,42 @@ time.sleep(4)
 
 login=driver.find_element(By.XPATH,"/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/div")
 login.click()  
-time.sleep(1)
+time.sleep(3)
 pele=driver.find_element(By.XPATH," /html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]")
 pele.click()
-time.sleep(2)
+time.sleep(3)
 pele=driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[7]/div/span/div/a/div")
 pele.click()
-time.sleep(2)
+time.sleep(3)
 pele=driver.find_element(By.XPATH, "/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[2]/div[1]/div/div/div[2]/div/button")
 pele.click
 
 
-time.sleep(1)
+time.sleep(2)
 pele=driver.find_element(By.XPATH,"/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[2]/div[1]/div/div/div[2]/div/button")
 pele.click()
 
+time.sleep(2)
 
 
+pyautogui.write(bilde) 
+time.sleep(2)
+pyautogui.press('Enter')
 
+time.sleep(2)
 
+pele=driver.find_element(By.XPATH,"/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div")
+pele.click()
+time.sleep(1)
 
+pele=driver.find_element(By.XPATH,"/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div")
+pele.click()
+time.sleep(1)
 
+pele=driver.find_element(By.XPATH,"/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[1]/div[2]/div/div[1]/div[1]")
+pele.send_keys(bildes_apraksts)
+time.sleep(1)
 
-
-
-
-
-
-
-
-
-
-
-input()
+pele=driver.find_element(By.XPATH,"/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div")
+pele.click()
+time.sleep(2)
